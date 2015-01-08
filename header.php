f8c0ace06fdeb70a05279f225665cd6b1f22032a
gomiebear <agomez@bailhotline.net>
2015-01-08 04:18:32 -0800

    styled nav-bar toggle collapsed, and added arrow to nav items

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Build/Join Your Ideal Team with LoL Talent Scout">
+    <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
     <meta name="author" content="Anthony Rivera & Anthony Gomez">
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/etalentscout.ico">
 
-    <title>LoL Talent Scout - Build/Join Your Ideal Team</title>
+    <title><?php echo get_bloginfo( 'title' ); ?></title>
     
     <?php wp_head(); ?>
     
@@ -26,24 +26,20 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="LoL Talent Scout Logo" title="LoL Talent Scout Logo"></a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          
-          <!--sign up and log in btn's -->
           <div class="navbar-btns pull-right">
-          		<a class="btn btn-clear" href="">Sign up</a>
-         		<a class="btn btn-clear" href="">Log in</a>
-          </div>
-      
-         
-		 <?php 
-		 	$args = array(
-				'menu'			=> 'header-menu',
-				'menu_class' 	=> 'nav navbar-nav pull-right',
-				'container'		=> 'false'
-			);
-			wp_nav_menu( $args );
-		 ?>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
+            <a class="btn btn-clear" href="">Sign up</a>
+            <a class="btn btn-clear" href="">Log in</a>
+          </div><!-- /.navbar-btns pull-right -->
+        </div><!-- /navbar-header -->
+	  </div><!-- /.container -->
+	  <div id="navbar" class="navbar-collapse collapse">
+	    <?php 
+          $args = array(
+             'menu'			=> 'header-menu',
+             'menu_class' 	=> 'nav navbar-nav',
+             'container'	=> 'false'
+          );
+          wp_nav_menu( $args );
+        ?>
+      </div><!--/.navbar-collapse -->
+	</nav>
